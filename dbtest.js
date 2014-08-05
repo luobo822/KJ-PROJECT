806821720f6421dde2c83e0b56d96b0835cf0869
suica <578970498@qq.com>
2014-08-05 15:29:24 +0800

    服务器端代码

--- dbtest.js
+++ dbtest.js
@@ -20,16 +20,13 @@ c.on('connect', function() {
    console.log('数据库连接关闭');
  });
 
-var test = {id:'233',username:'233',baka:'244'};
+var test = {id:'233',username:'233'};
+var TABLE = 'user'; 
 
-c.query('SELECT COUNT(username)=0 FROM user WHERE id = :id AND username = :username',test)
+c.query('SELECT * FROM '+TABLE+' WHERE username = :username',test)
  .on('result', function(res) {
    res.on('row', function(row) {
     console.log('Result row: ' + inspect(row));
-     for (x in row)
-      {
-        console.log(row[x]);
-      }
    })
    .on('error', function(err) {
      console.log('结果输出错误: ' + inspect(err));
@@ -42,33 +39,33 @@ c.query('SELECT COUNT(username)=0 FROM user WHERE id = :id AND username = :usern
    console.log('所有结果输出完毕');
  });
 
-var data = new Object();
-data = {
-  id:'2',
-  username:'3',
-  password:'4',
-  email:'5',
-  qq:'6',
-  nickname:'7',
-  sq:'8',
-  isq:'9'
-};
-
-
-c.query('INSERT INTO user SET id = ?, username = ?, password = ?, email = ?, qq = ?, nickname = ?, sq = ?, isq = ?',[data.id,data.username,data.password,data.email,data.qq,data.nickname,data.sq,data.isq])
- .on('result', function(res) {
-   res.on('row', function(row) {
-     console.log('插入成功！');
-   })
-   .on('error', function(err) {
-     console.log('Result error: ' + inspect(err));
-   })
-   .on('end', function(info) {
-     console.log('Result finished successfully');
-   });
- })
- .on('end', function() {
-   console.log('Done with all results');
- });
-
 c.end();
+// var data = new Object();
+// data = {
+//   id:'2',
+//   username:'3',
+//   password:'4',
+//   email:'5',
+//   qq:'6',
+//   nickname:'7',
+//   sq:'8',
+//   isq:'9'
+// };
+
+
+// c.query('INSERT INTO user SET id = ?, username = ?, password = ?, email = ?, qq = ?, nickname = ?, sq = ?, isq = ?',[data.id,data.username,data.password,data.email,data.qq,data.nickname,data.sq,data.isq])
+//  .on('result', function(res) {
+//    res.on('row', function(row) {
+//      console.log('插入成功！');
+//    })
+//    .on('error', function(err) {
+//      console.log('Result error: ' + inspect(err));
+//    })
+//    .on('end', function(info) {
+//      console.log('Result finished successfully');
+//    });
+//  })
+//  .on('end', function() {
+//    console.log('Done with all results');
+//  });
+
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-var fs = require("fs")
-    , http = require("http")
+var fs = require('fs')
+    , http = require('http')
 
 var inspect = require('util').inspect;
-var socketio = require("socket.io");
+var socketio = require('socket.io');
 var Client = require('mariasql');
 var c = new Client();
 
     
 
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-type": "text/html"});
-    res.end("please view this page through your app\n");
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end('please view this page through your app\n');
 }).listen(2333, function() {
-    console.log("处理register/login/message/main 监听: http://157.7.138.169:2333");    
+    console.log('处理register/login/message/main 监听: http://157.7.138.169:2333');    
 });
 
     c.connect({
@@ -33,17 +33,17 @@ var server = http.createServer(function(req, res) {
         console.log('数据库连接关闭');
     });
 
-socketio.listen(server).on("connection", function (socket) {
+socketio.listen(server).on('connection', function (socket) {
     
-    socket.on("message", function (msg) {
-        console.log("收到消息:", msg);
-        socket.broadcast.emit("message", msg);
+    socket.on('message', function (msg) {
+        console.log('收到消息:', msg);
+        socket.broadcast.emit('message', msg);
     });
 
-    socket.on("reg_check_username_server", function (uzn) {
-        console.log("收到需检查的用户名:",uzn);
+    socket.on('reg_check_username_server', function (uzn) {
+        console.log('收到需检查的用户名:',uzn);
         var is_checked = -1;
-        c.query('SELECT COUNT(username)=0 FROM user WHERE username = :username',uzn) //检查数据库，在没有重名的时候输出1
+        c.query('SELECT COUNT(username)=0 FROM user WHERE username = :username',uzn) //检查数据库,在没有重名的时候输出1
          .on('result', function(res) {
            res.on('row', function(row) {
             console.log('查到的结果是:' + inspect(row));
@@ -54,7 +54,7 @@ socketio.listen(server).on("connection", function (socket) {
                 }else {
                   console.log('用户名重复了:'+uzn.username);
                 };
-                socket.emit("reg_check_username_client", is_checked); //重名向客户端发送0，未重名发送1
+                socket.emit('reg_check_username_client', is_checked); //重名向客户端发送0,未重名发送1
               };
            })
            .on('error', function(err) {
@@ -68,9 +68,11 @@ socketio.listen(server).on("connection", function (socket) {
            console.log('所有结果输出完毕');
          });
     });
+    
 
-    socket.on("reg_submit_server", function (data) {
-        console.log("收到提交的新用户注册表单对象:\n",data);
+
+    socket.on('reg_submit_server', function (data) {
+        console.log('收到提交的新用户注册表单对象:\n',data);
 
         c.query('INSERT INTO user SET id = ?, username = ?, password = ?, email = ?, qq = ?, nickname = ?, sq = ?, isq = ?',[data.id,data.username,data.password,data.email,data.qq,data.nickname,data.sq,data.isq])
          .on('result', function(res) {
@@ -87,9 +89,56 @@ socketio.listen(server).on("connection", function (socket) {
          .on('end', function() {
            console.log('所有结果输出完毕');
          });
-        console.log("返回:",data);
-        socket.emit("reg_submit_client",data); //如果成功加入数据，返回对象.
+        console.log('返回:',data);
+        socket.emit('reg_submit_client',data); //如果成功插入数据,返回对象.
+    });    
+
+    socket.on('login_server', function (logindata) {
+        console.log('收到登录data:',logindata);
+        var is_right = -1;
+        c.query('SELECT COUNT(username)=1 FROM user WHERE id = :id AND username = :username',logindata)
+         .on('result', function(res) {
+           res.on('row', function(row) {
+            console.log('查到的结果是:' + inspect(row));
+            for (x in row){
+                is_right = row[x];
+                if (row[x] == 1) { //若id和用户名正确,进行第二遍查询,查询出nickname
+                  console.log('允许登录'+logindata.username);
+                  c.query('SELECT * FROM user WHERE id = :id AND username = :username',logindata)
+                     .on('result', function(res) {
+                       res.on('row', function(row) {
+                        row.flag = is_right;
+                        console.log('输出row'+row);
+                        socket.emit('login_client',row); //允许登录发送1/不允许登录发送0/错误发送-1
+                       })
+                       .on('error', function(err) {
+                         console.log('结果输出错误: ' + inspect(err));
+                       })
+                       .on('end', function(info) {
+                         console.log('结果输出成功');
+                       });
+                     })
+                     .on('end', function() {
+                       console.log('所有结果输出完毕');
+                     });
+                }else {
+                  console.log('不允许登录,没有此用户或密码错误:'+logindata.username);
+                  row.flag = 0;
+                  socket.emit('login_client',row); //允许登录发送1/不允许登录发送0/错误发送-1
+                };
+              };
+           })
+           .on('error', function(err) {
+             console.log('登录程序发生异常错误:' + inspect(err));
+           })
+           .on('end', function(info) {
+             console.log('登录处理完毕');
+           });
+         })
+         .on('end', function() {
+           console.log('所有结果输出完毕');
+         });
+    });
 
         
-    });
 });
