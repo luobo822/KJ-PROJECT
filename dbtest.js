51977ceffa12dc4d98d05f9f84d71ae81b6420bc
suica <578970498@qq.com>
2014-08-07 11:21:21 +0800

    server VER 0.5
    开始进行推送尝试

--- dbtest.js
+++ dbtest.js
@@ -20,40 +20,33 @@ c.on('connect', function() {
    console.log('数据库连接关闭');
  });
 
-var test = {id:'233',username:'233'};
-var TABLE = 'user'; 
+// var test = {id:'233',username:'233'};
+// var TABLE = 'test'; 
 
-c.query('SELECT * FROM '+TABLE+' WHERE username = :username',test)
- .on('result', function(res) {
-   res.on('row', function(row) {
-    console.log('Result row: ' + inspect(row));
-   })
-   .on('error', function(err) {
-     console.log('结果输出错误: ' + inspect(err));
-   })
-   .on('end', function(info) {
-     console.log('结果输出成功');
-   });
- })
- .on('end', function() {
-   console.log('所有结果输出完毕');
- });
+// c.query('SELECT * FROM '+TABLE+' WHERE username = :username',test)
+//  .on('result', function(res) {
+//    res.on('row', function(row) {
+//     console.log('Result row: ' + inspect(row));
+//    })
+//    .on('error', function(err) {
+//      console.log('结果输出错误: ' + inspect(err));
+//    })
+//    .on('end', function(info) {
+//      console.log('结果输出成功');
+//    });
+//  })
+//  .on('end', function() {
+//    console.log('所有结果输出完毕');
+//  });
 
-c.end();
-// var data = new Object();
-// data = {
-//   id:'2',
-//   username:'3',
-//   password:'4',
-//   email:'5',
-//   qq:'6',
-//   nickname:'7',
-//   sq:'8',
-//   isq:'9'
-// };
+// var data = new Array();
+
+// data = [1,2,'笨蛋'];
 
 
-// c.query('INSERT INTO user SET id = ?, username = ?, password = ?, email = ?, qq = ?, nickname = ?, sq = ?, isq = ?',[data.id,data.username,data.password,data.email,data.qq,data.nickname,data.sq,data.isq])
+// c.query('INSERT INTO test SET id = ?, username = ?,'+
+//   'baka = ?',
+//   data)
 //  .on('result', function(res) {
 //    res.on('row', function(row) {
 //      console.log('插入成功！');
@@ -68,4 +61,42 @@ c.end();
 //  .on('end', function() {
 //    console.log('Done with all results');
 //  });
+// UserName=@UserName
+//
+
+
+ c.query('SELECT rowid FROM test')
+ .on('result', function(res) {
+   res.on('row', function(row) {
+        for(x in row){
+          console.log(row[x]);
+        };
+   })
+   .on('error', function(err) {
+     console.log(err['code']);
+   })
+   .on('end', function(info) {
+     console.log('Result finished successfully');
+   });
+ })
+ .on('end', function() {
+   console.log('Done with all results');
+ });
+c.end();
+
+ // c.query('ALTER TABLE test ADD text')
+ // .on('result', function(res) {
+ //   res.on('row', function(row) {
+ //     console.log(inspect(row));
+ //   })
+ //   .on('error', function(err) {
+ //     console.log('Result error: ' + inspect(err));
+ //   })
+ //   .on('end', function(info) {
+ //     console.log('Result finished successfully');
+ //   });
+ // })
+ // .on('end', function() {
+ //   console.log('Done with all results');
+ // });
 
--- server.js
+++ server.js
@@ -225,10 +225,40 @@ socketio.listen(server).on('connection', function (socket) {
            console.log('main_csv_submit_server:所有结果输出完毕');
          });
 
+
+
+
     });
 
+    socket.on('main_mission_server', function (nickname) {
+      console.log('main_mission_server:收到任务刷新请求,请求的发送用户是', nickname);
+      c.query('SELECT i2,'+nickname+' FROM relation')
+       .on('result', function(res) {
+         res.on('row', function(row) {
+          console.log('main_mission_server:查询成功'+inspect(row));
+          if (row[nickname]) {
+            socket.emit('main_mission_client',row);
+          };
+         })
+         .on('error', function(err) {
+           console.log('main_mission_server:发生异常错误:' + inspect(err));
+           socket.emit('main_mission_client',-1);
+         })
+         .on('end', function(info) {
+           console.log('main_mission_server:推送完毕');
+         })
+       })
+       .on('end', function() {
+         console.log('main_mission_server:结果输出完毕');
+       });
+      
+    }); //socket.on('main_mission_server') ending 
+
+//函数要异步太电波了!
+
+
 function insert_itemdata(csvdata){
-    c.query('INSERT INTO itemdata SET i1 = ?, i2 = ?, i3 = ?, i4 = ?, i5 = ?, i6 = ?, i7 = ?, i8 = ?, i9 = ?, i10 = ?, i11 = ?, i12 = ?,'+
+c.query('INSERT INTO itemdata SET i1 = ?, i2 = ?, i3 = ?, i4 = ?, i5 = ?, i6 = ?, i7 = ?, i8 = ?, i9 = ?, i10 = ?, i11 = ?, i12 = ?,'+
         'i13 = ?, i14 = ?, i15 = ?, i16 = ?, i17 = ?, i18 = ?, i19 = ?, i20 = ?, i21 = ?, i22 = ?, i23 = ?, i24 = ?, i25 = ?, i26 = ?, i27 =?',
          csvdata)
          .on('result', function(res) {
@@ -374,4 +404,37 @@ c.query('SELECT COUNT('+columnname+')=0 FROM '+tablename+' '+condition)
 
 // };
         
+
+//SQL模板
+
+// c.query('')
+//          .on('result', function(res) {
+//            res.on('row2', function(row2) {
+//             console.log('成功');
+//            })
+//            .on('error', function(err) {
+//              console.log('发生异常错误:' + inspect(err));
+//            })
+//            .on('end', function(info) {
+//              console.log('完毕');
+//            })
+//          })
+//          .on('end', function() {
+//            console.log('结果输出完毕');
+//          });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
