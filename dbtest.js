42d9dcd4218ca7c865163139209736f0fcc645a0
suica <578970498@qq.com>
2014-10-18 14:02:34 +0800

    数据库小测试

--- dbtest.js
+++ dbtest.js
@@ -21,7 +21,8 @@ c.on('connect', function() {
  });
 
 var a = '`'+"test1213123"+'`';
- c.query('CREATE TABLE IF NOT EXISTS '+a+' ( i1 text ,i2 text ,i3 text ,i4 text ,i5 text ,i6 text ,i7 text ,i8 text ,i9 text ,i10 text ,i11 text ,i12 text ,i13 text ,i14 text ,i15 text ,i16 text ,i17 text ,i18 text ,i19 text ,i20 text ,i21 text ,i22 text ,i23 text ,i24 text ,i25 text ,i26 text ,i27 text)CHARSET=utf8')
+var b = 'baka';
+ c.query('UPDATE test SET '+b+'= '+a+' WHERE rowid = ?',[3])
  .on('result', function(res) {
    res.on('row', function(row) {
      console.log(inspect(row));
